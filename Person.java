acfc579e86db2e74660dbe670f3864c9d527f9af
lonelypenguin245 <144401532+lonelypenguin245@users.noreply.github.com>
2023-10-17 08:53:48 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package StudentInfo;
+
+/**
+ *
+ * @author HI
+ */
+public class Person {
+    String name;
+    String address;
+    double salary;
+
+    public Person() {
+    }
+
+    public Person(String name, String address, double salary) {
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    void displayPersonInfo() {
+        System.out.println("Information of Person you have entered: ");
+        System.out.println("Name: " + this.name);
+        System.out.println("Address: " + this.address);
+        System.out.println("Salary: " + this.salary);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
